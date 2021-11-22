f221a24f3ec5ef939f42c630bdc5e5841e4a9543
Dieyson Rander <dieyson99103660@gmail.com>
2021-11-22 09:20:50 -0300

    Formatação de dados e inicio das validaçôes.

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace SalesWebMvc.Models
@@ -7,11 +8,27 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage ="O Nome deve ser preenchido")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "O {0} deve ter no mínimo {1} e no máximo {2} caracteres.")]
         public string Name { get; set; }
+
+        [Display(Name = "E-mail")]
+        [Required]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
+        [Display(Name = "Salário Base")]
+        [DataType(DataType.Currency)]
         public double BaseSalary { get; set; }
+
+        [Display(Name = "Data de Aniversário")]
+        [DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
         public int DepartmentId { get; set; }
+
+        [Display(Name = "Departamento")]
         public Department Department { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
